fd919705887334fedabd412657092b20b0f7c9b7
Ben Bishop <ben@freckleinteractive.net>
2013-04-19 14:40:51 -0500

    Setup initial test UI.

--- RazorSVG/MainActivity.cs
+++ RazorSVG/MainActivity.cs
@@ -14,6 +14,10 @@ namespace RazorSVG
 	{
 		int count = 1;
 
+		protected SeekBar CurrentValueSeekBar, UpdateSpeedSeekBar;
+
+		protected TextView CurrentValueLabel, UpdateSpeedLabel;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
@@ -21,13 +25,57 @@ namespace RazorSVG
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+			//initalizing UI
+			InitLabelReferences ();
+			InitCurrentValueSeekBar ();
+			InitUpdateSpeedSeekBar ();
+
+			//initial updating of labels
+			UpdateSpeedLabelText();
+			UpdateCurrentValueLabelText();
+
+		}
+
+		protected void InitLabelReferences ()
+		{
+			CurrentValueLabel = FindViewById<TextView> (Resource.Id.currentValueLabel);
+			UpdateSpeedLabel = FindViewById<TextView> (Resource.Id.updateSpeedLabel);
+		}
+
+		protected void InitUpdateSpeedSeekBar ()
+		{
+			UpdateSpeedSeekBar = FindViewById<SeekBar> (Resource.Id.updateSpeedSeekBar);
+			UpdateSpeedSeekBar.Max = 60;
+			UpdateSpeedSeekBar.Progress = 24;
+			UpdateSpeedSeekBar.ProgressChanged += HandleUpdateSpeedChanged;
+		}
+
+
+		protected void InitCurrentValueSeekBar ()
+		{
+			CurrentValueSeekBar = FindViewById<SeekBar> (Resource.Id.currentValueSeekBar);
+			CurrentValueSeekBar.Max = 100;
+			CurrentValueSeekBar.Progress = 50;
+			CurrentValueSeekBar.ProgressChanged += HandleCurrentValueChanged;
+		}
+
+		protected void UpdateCurrentValueLabelText ()
+		{
+			CurrentValueLabel.Text = String.Format("Current Value ({0})", CurrentValueSeekBar.Progress.ToString());
+		}
+
+		protected void UpdateSpeedLabelText ()
+		{
+			UpdateSpeedLabel.Text = String.Format("Update Speed ({0})", UpdateSpeedSeekBar.Progress.ToString());
+		}
+
+		protected void HandleCurrentValueChanged (object sender, SeekBar.ProgressChangedEventArgs e)
+		{
+			UpdateCurrentValueLabelText();
+		}
+		protected void HandleUpdateSpeedChanged (object sender, SeekBar.ProgressChangedEventArgs e)
+		{
+			UpdateSpeedLabelText();
 		}
 	}
 }
--- RazorSVG/Resources/Resource.designer.cs
+++ RazorSVG/Resources/Resource.designer.cs
@@ -60,8 +60,23 @@ namespace RazorSVG
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050002
+			public const int currentValueLabel = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int currentValueSeekBar = 2131034115;
+			
+			// aapt resource value: 0x7f050001
+			public const int svgWebView = 2131034113;
+			
 			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050004
+			public const int updateSpeedLabel = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int updateSpeedSeekBar = 2131034117;
 			
 			static Id()
 			{
